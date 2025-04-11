8e9e3669acbf71be5509ec0c7ad76d0c43786779
Christian Schudoma <christian.schudoma@embl.de>
2025-04-11 16:10:16 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ here = path.abspath(path.dirname("__file__"))
 with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
 	description = long_description = description.read()
 
-	name="MGExpose"
+	name="mgexpose"
 	version = mgexpose_version
 
 	if sys.version_info.major != 3:
