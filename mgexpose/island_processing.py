5a3944e5a0e4280fe216aeada4422441a470cfbe
Christian Schudoma <christian.schudoma@embl.de>
2025-05-13 10:00:37 +0200

    gene objects now have a parent id

--- mgexpose/island_processing.py
+++ mgexpose/island_processing.py
@@ -42,6 +42,7 @@ def check_island_genes(genes, precomputed_islands=None):
                         gene.speci = {island.name}
                     else:
                         gene.speci.add(island.name)
+                    gene.parent = island.name
                     island.add_gene(gene)
 
                 logger.info("%s %s", log_str, str(add_gene))
