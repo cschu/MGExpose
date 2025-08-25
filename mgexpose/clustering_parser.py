4d3bb0152ab0d92ebbdacd391e90d96edd0724a5
Christian Schudoma <christian.schudoma@embl.de>
2025-08-25 13:44:40 +0200

    versose evaluate_y_clusters

--- mgexpose/clustering_parser.py
+++ mgexpose/clustering_parser.py
@@ -183,15 +183,18 @@ def parse_y_clusters(cluster_data, genes):
 
 
 def evaluate_y_clusters(cluster_data, genes, core_threshold=0.95,):
+    print("EVALUATE_Y_CLUSTERS")
     for line in get_lines_from_chunks(cluster_data):
         last_cluster, float_frac = None, None
         gene_id, cluster, _, _, _, frac = line.strip().split("\t")
         if cluster != last_cluster:
             last_cluster, float_frac = cluster, float(frac)
         gene = genes.get(gene_id)
+        print(gene_id, cluster, frac, "->", gene)
         if gene is not None:
             gene.cluster = f"{cluster}:{frac}"
             gene.is_core = float_frac > core_threshold
+            print("===>", gene)
         
         
 
